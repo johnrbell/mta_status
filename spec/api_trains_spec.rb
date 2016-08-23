cb026a6433fab13a35eb1c7c1b5c2ebb4004cbdf
Bryan Mytko <bryanmytko@gmail.com>
2016-08-23 14:48:17 -0400

    Update context descriptionn for api/trains specs

--- spec/api_trains_spec.rb
+++ spec/api_trains_spec.rb
@@ -3,17 +3,13 @@ require "spec_helper"
 describe "Trains API" do
   describe "GET /trains" do
     it "responds with a 200" do
-      VCR.use_cassette("api/trains") do
-        get "/api/trains"
-      end
+      VCR.use_cassette("api/trains") { get "/api/trains" }
 
       expect(last_response.status).to eq 200
     end
 
     it "responds with train data" do
-      VCR.use_cassette("api/trains") do
-        get "/api/trains"
-      end
+      VCR.use_cassette("api/trains") { get "/api/trains" }
 
       response = JSON.parse(last_response.body)
       expect(response).to_not be_empty
@@ -21,26 +17,22 @@ describe "Trains API" do
   end
 
   describe "GET /trains/:id" do
-    context "when the train is valid" do
+    context "when the id is valid" do
       it "responds with a 200" do
-        VCR.use_cassette("api/trains/:id") do
-          get "/api/trains/F"
-        end
+        VCR.use_cassette("api/trains/:id") { get "/api/trains/F" }
 
         expect(last_response.status).to eq 200
       end
 
       it "responds with train data" do
-        VCR.use_cassette("api/trains/:id") do
-          get "/api/trains/F"
-        end
+        VCR.use_cassette("api/trains/:id") { get "/api/trains/F" }
 
         status = JSON.parse(last_response.body)["status"]
         expect(status).to_not be_nil
       end
     end
 
-    context "when the train is invalid" do
+    context "when the id is invalid" do
       it "responds with a 404" do
         VCR.use_cassette("api/trains/invalid") do
           get "/api/trains/invalid_train"
