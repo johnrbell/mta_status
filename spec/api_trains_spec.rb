6ddd2cf8864e2163fbc19805eb0b1daff6e0fe7c
Bryan Mytko <bryanmytko@gmail.com>
2016-08-23 12:33:18 -0400

    Add VCR to api trains specs

--- spec/api_trains_spec.rb
+++ spec/api_trains_spec.rb
@@ -3,42 +3,50 @@ require "spec_helper"
 describe "Trains API" do
   describe "GET /trains" do
     it "responds with a 200" do
-      get "/api/trains"
+      VCR.use_cassette("api/trains") do
+        get "/api/trains"
+      end
+
       expect(last_response.status).to eq 200
     end
 
     it "responds with train data" do
-      get "/api/trains"
+      VCR.use_cassette("api/trains") do
+        get "/api/trains"
+      end
+
       response = JSON.parse(last_response.body)
       expect(response).to_not be_empty
     end
   end
 
-  describe "GET /trains/:id" do
-    context "when the train is valid" do
-      it "responds with a 200" do
-        get "/api/trains/F"
-        expect(last_response.status).to eq 200
-      end
-
-      it "responds with train data" do
-        get "/api/trains/F"
-        status = JSON.parse(last_response.body)["status"]
-        expect(status).to_not be_nil
-      end
-    end
-
-    context "when the train is invalid" do
-      it "responds with a 404" do
-        get "/api/trains/invalid_train"
-        expect(last_response.status).to eq 404
-      end
-
-      it "responds with an error" do
-        get "/api/trains/invalid_train"
-        error = JSON.parse(last_response.body)["error"]
-        expect(error).to_not be_nil
-      end
-    end
-  end
+  # describe "GET /trains/:id" do
+  #   VCR.use_cassette("api/trains/:id") do
+  #     context "when the train is valid" do
+  #       it "responds with a 200" do
+  #         get "/api/trains/F"
+  #         expect(last_response.status).to eq 200
+  #       end
+  #
+  #       it "responds with train data" do
+  #         get "/api/trains/F"
+  #         status = JSON.parse(last_response.body)["status"]
+  #         expect(status).to_not be_nil
+  #       end
+  #     end
+  #
+  #     context "when the train is invalid" do
+  #       it "responds with a 404" do
+  #         get "/api/trains/invalid_train"
+  #         expect(last_response.status).to eq 404
+  #       end
+  #
+  #       it "responds with an error" do
+  #         get "/api/trains/invalid_train"
+  #         error = JSON.parse(last_response.body)["error"]
+  #         expect(error).to_not be_nil
+  #       end
+  #     end
+  #   end
+  # end
 end
