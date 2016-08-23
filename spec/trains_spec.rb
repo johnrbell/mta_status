61fee4e112900aab07cfb9e1ac4998b8858fc667
Bryan Mytko <bryanmytko@gmail.com>
2016-08-23 14:47:47 -0400

    Add /trains specs

--- spec/trains_spec.rb
+++ spec/trains_spec.rb
@@ -1,7 +1,35 @@
 require "spec_helper"
 
 describe "Trains" do
-  it "works" do
-    expect(true).to be true
+  describe "GET /trains" do
+    it "responds with a 200" do
+      VCR.use_cassette("trains") do
+        get "/"
+      end
+
+      expect(last_response.status).to eq 200
+    end
+  end
+
+  describe "GET /trains/:id" do
+    context "when the id is valid" do
+      it "responds with a 200" do
+        VCR.use_cassette("trains") do
+          get "/trains/F"
+        end
+
+        expect(last_response.status).to eq 404
+      end
+    end
+
+    context "when the id is invalid" do
+      it "responds with a 404" do
+        VCR.use_cassette("trains/invalid") do
+          get "/trains/invalid_train"
+        end
+
+        expect(last_response.status).to eq 404
+      end
+    end
   end
 end
