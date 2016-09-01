f60a5ee1ce405234aa68e791059f1b86f4c7fd46
John Bell <Johnrbell@gmail.com>
2016-09-01 15:30:22 -0400

    fighting with some production bug

--- helpers/get_bg_image.rb
+++ helpers/get_bg_image.rb
@@ -37,7 +37,26 @@ module GetBGImage
 
   # TODO: save locally 
   def default_images
-    ["https://images.unsplash.com/photo-1470290032981-3371c20736f6?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=2874c6f4523d7302fc5a2f7809613ec6"]
+    [# factory
+    "https://images.unsplash.com/photo-1470290032981-3371c20736f6?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=2874c6f4523d7302fc5a2f7809613ec6",
+    # esb
+    "https://images.unsplash.com/photo-1465487438571-59340bfc35dd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=9fa78486dbc49708bb377a49a031bd77",
+    # flatiron
+    "https://images.unsplash.com/photo-1433437728106-854c6e19699c?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=e7f56fbd788432d64c5101e50d6bd8ed",
+    # wtc
+    "https://images.unsplash.com/photo-1467250123231-1813550b3fd5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=ea3c6bd57306b0616d91507cdfdc4638",
+    # flatiron 2
+    "https://images.unsplash.com/photo-1428976365951-b70e0fa5c551?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=e81a3dab508c7c545fb32e1cf0339d96",
+    # cabs
+    "https://images.unsplash.com/39/mtNrf7oxS4uSxTzMBWfQ_DSC_0043.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=3f50188f1e5cf13b10f4b0492ae6ac1f",
+    # chrysler
+    "https://images.unsplash.com/photo-1463267511177-6ae5f8c670d4?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=da09a211ea3ef975c1f3259d328381bb",
+    # brooklyn bridge
+    "https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=8f72e46744fd8958c1d992d53062eaaa",
+    # street
+    "https://images.unsplash.com/photo-1466500419182-8602dc906b51?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=0d406540cd55d48f045186106dd0c0fb",
+    # park
+    "https://images.unsplash.com/photo-1464225495945-af130cc9f19e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=1200&fit=crop&s=cf114e43f8289edc5e821d13ea08deff"]
   end
 
   module_function :get_bg_img
