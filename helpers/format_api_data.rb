1794724df12cc2570b6d1edf6eff4ea46462d0cc
Jared <jaredcmurphy@gmail.com>
2016-09-07 18:07:31 -0400

    refactor helper method with inject

--- helpers/format_api_data.rb
+++ helpers/format_api_data.rb
@@ -1,17 +1,28 @@
 module FormatApiData
 
   def format_api_data(trains_data)
-    formatted_data = []
-    trains_data.each do |line|
-      line[:name].split("").each do |train|
-        formatted_data.push({
+
+    trains_data.inject([]) do |prev, curr|
+      curr[:name].split("").each do |train|
+        prev.push({
           :name => train,
-          :status => line[:status],
-          :long_status => line[:long_status]
+          :status => curr[:status],
+          :long_status => curr[:long_status]
         })
       end
+      prev
     end
-    formatted_data
+    # formatted_data = []
+    # trains_data.each do |line|
+    #   line[:name].split("").each do |train|
+    #     formatted_data.push({
+    #       :name => train,
+    #       :status => line[:status],
+    #       :long_status => line[:long_status]
+    #     })
+    #   end
+    # end
+    #formatted_data
   end
 
 end
