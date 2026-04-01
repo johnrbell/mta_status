77f8ae44169e3888d8abd9447aa4f57ef12d895f
John Bell <johnrbell@gmail.com>
2026-04-01 14:19:28 -0400

    Remove build output from repo, add .vercel to gitignore

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-import * as server from '../entries/pages/_name_/_page.server.js';
-
-export const index = 3;
-export { server };
-export const server_id = "src/routes/[name]/+page.server.js";
-export const imports = [];
-export const stylesheets = [];
-export const fonts = [];
